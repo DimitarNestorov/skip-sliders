d79a049e38de629667560af30f5dcc8245e25af3
Dimitar Nestorov <8790386+DimitarNestorov@users.noreply.github.com>
2026-08-14 00:40:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,8 +11,16 @@ let package = Package(
     products: [
         .library(name: "Sliders", targets: ["Sliders"])
     ],
+    dependencies: [
+		.package(url: "https://source.skip.tools/skip.git", from: "1.9.5"),
+    ],
     targets: [
-        .target(name: "Sliders"),
+        .target(
+            name: "Sliders",    
+			plugins: [
+				.plugin(name: "skipstone", package: "skip"),
+			],
+        ),
         .testTarget(name: "SlidersTests", dependencies: ["Sliders"])
     ]
 )
