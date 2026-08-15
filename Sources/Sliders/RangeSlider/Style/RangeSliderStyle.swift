7a257c2015586247d39a9d4bcb7f9a78e0a5df8f
Dimitar Nestorov <8790386+DimitarNestorov@users.noreply.github.com>
2026-08-15 23:08:29 +0300

    AnyView

--- Sources/Sliders/RangeSlider/Style/RangeSliderStyle.swift
+++ Sources/Sliders/RangeSlider/Style/RangeSliderStyle.swift
@@ -17,7 +17,7 @@ public protocol RangeSliderStyle {
     ///
     /// This method will be called for each instance of `RangeSlider` created within
     /// a view hierarchy where this style is the current `RangeSliderStyle`.
-    func makeBody(configuration: Self.Configuration) -> Self.Body
+    func makeBody(configuration: Self.Configuration) -> AnyView
 
     /// The properties of a `RangeSlider` instance being created.
     typealias Configuration = RangeSliderStyleConfiguration
